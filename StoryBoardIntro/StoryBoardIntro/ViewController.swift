4aef502c35ebe5f21100089c8634670bde4e99f7
sonal-bhatia <69254430+sonal-bhatia@users.noreply.github.com>
2020-08-05 11:33:33 -0400

    My First Xcode Project

---
+++ StoryBoardIntro/StoryBoardIntro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoryBoardIntro
+//
+//  Created by Sonal Bhatia on 8/5/20.
+//  Copyright © 2020 Sonal Bhatia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is a print statement - testing testing!")
+    }
+
+
+}
+
